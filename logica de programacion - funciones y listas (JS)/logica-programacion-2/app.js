b906fcf8afc9d6112acb505cdb606c7d3163bdd2
Robinson Aparicio <apariciodev1812@gmail.com>
2024-08-14 14:31:05 -0500

    Add: Curso 2

---
+++ logica de programacion - funciones y listas (JS)/logica-programacion-2/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector("h1")
+title.innerHTML = "Juego del número secreto"
+
+let parrafo = document.querySelector(".texto__parrafo")
+parrafo.innerHTML = "Escoge un número del 1 al 9"
+
+function llamarUsuario(){
+  alert("llamada desde el archivo .js")
+}
