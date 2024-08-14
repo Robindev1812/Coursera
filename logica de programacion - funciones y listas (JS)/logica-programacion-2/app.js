b8cba06beb4f1feab9e3b8789c503b1f08e4a783
Robinson Aparicio <apariciodev1812@gmail.com>
2024-08-14 14:57:50 -0500

    delete: files

--- logica de programacion - funciones y listas (JS)/logica-programacion-2/app.js
+++
@@ -1,9 +0,0 @@
-let title = document.querySelector("h1")
-title.innerHTML = "Juego del número secreto"
-
-let parrafo = document.querySelector(".texto__parrafo")
-parrafo.innerHTML = "Escoge un número del 1 al 9"
-
-function llamarUsuario(){
-  alert("llamada desde el archivo .js")
-}
